3bf848e30a07f911871f40494c473c521e243dae
eatandenjoy <34268973+eatandenjoy@users.noreply.github.com>
2017-12-22 14:12:56 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(function() {
+  $('nav a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('active');
+});
+$(function(){
+	$('a[data-target^="anchor"]').bind('click.smoothscroll', function(){
+		var target = $(this).attr('href'),
+			bl_top = $(target).offset().top - 57;
+		$('body, html').animate({scrollTop: bl_top}, 700);
+		return false;
+	});
+});
